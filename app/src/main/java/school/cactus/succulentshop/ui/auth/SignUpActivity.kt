9ac9250f6657a82173ee77817ca24eb09690b49f
turkergoksu <turkergoksu@gmail.com>
2021-04-30 17:30:52 +0300

    Implemented EmailValidator.kt

--- app/src/main/java/school/cactus/succulentshop/ui/auth/SignUpActivity.kt
+++ app/src/main/java/school/cactus/succulentshop/ui/auth/SignUpActivity.kt
@@ -2,11 +2,17 @@ package school.cactus.succulentshop.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import school.cactus.succulentshop.R
 import school.cactus.succulentshop.databinding.ActivitySignUpBinding
+import school.cactus.succulentshop.ui.auth.validators.EmailValidator
 
 class SignUpActivity : AppCompatActivity() {
     private var binding: ActivitySignUpBinding? = null
+
+    private val emailValidator by lazy {
+        EmailValidator()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +22,25 @@ class SignUpActivity : AppCompatActivity() {
 
         binding?.apply {
             signUpButton.setOnClickListener {
-                // TODO: 30-Apr-21
+                emailTextInputLayout.validate()
             }
 
             alreadyHaveAnAccountButton.setOnClickListener {
                 finish()
             }
         }
+    }
+
+    private fun TextInputLayout.validate() {
+        val errorMessage = validator().validate(editText!!.text.toString())
+        error = errorMessage?.resolveAsString()
+        isErrorEnabled = errorMessage != null
+    }
+
+    private fun Int.resolveAsString() = getString(this)
+
+    private fun TextInputLayout.validator() = when (this) {
+        binding?.emailTextInputLayout -> emailValidator
+        else -> throw IllegalArgumentException("Cannot find any validator for the given TextInputLayout")
     }
 }
