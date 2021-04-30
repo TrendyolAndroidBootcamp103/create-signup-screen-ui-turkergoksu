f2d5612f1ca8c56577e41b9fd8c3c305466f6f92
turkergoksu <turkergoksu@gmail.com>
2021-04-30 23:12:32 +0300

    Added EmailValidatorTest.kt

---
+++ app/src/test/java/school/cactus/succulentshop/auth/EmailValidatorTest.kt
@@ -0,0 +1,78 @@
+package school.cactus.succulentshop.auth
+
+import com.google.common.truth.Truth
+import org.junit.Test
+import school.cactus.succulentshop.R
+import school.cactus.succulentshop.ui.auth.validators.EmailValidator
+
+class EmailValidatorTest {
+    @Test
+    fun `given empty e-mail as input, when validate is called, then validate should return err_email_field_is_required`() {
+        // Given
+        val givenEmail = ""
+        val emailValidator = EmailValidator()
+
+        // When
+        val actualResult = emailValidator.validate(givenEmail)
+
+        // Then
+        val expectedResult = R.string.err_email_field_is_required
+        Truth.assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun `given invalid e-mail as input, when validate is called, then validate should return err_email_is_invalid`() {
+        // Given
+        val givenEmail = "turkergoksugmail.com"
+        val emailValidator = EmailValidator()
+
+        // When
+        val actualResult = emailValidator.validate(givenEmail)
+
+        // Then
+        val expectedResult = R.string.err_email_is_invalid
+        Truth.assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun `given short e-mail as input, when validate is called, then validate should return err_email_is_invalid`() {
+        // Given
+        val givenEmail = "t@g.c"
+        val emailValidator = EmailValidator()
+
+        // When
+        val actualResult = emailValidator.validate(givenEmail)
+
+        // Then
+        val expectedResult = R.string.err_email_is_invalid
+        Truth.assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun `given long e-mail as input, when validate is called, then validate should return err_email_is_invalid`() {
+        // Given
+        val givenEmail = "AAAAAbbbbbAAAAAbbbbbAAAAAbbbbbAAAAAbbbbbAAAAAbbbbb@gmail.com"
+        val emailValidator = EmailValidator()
+
+        // When
+        val actualResult = emailValidator.validate(givenEmail)
+
+        // Then
+        val expectedResult = R.string.err_email_is_invalid
+        Truth.assertThat(actualResult).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun `given valid e-mail as input, when validate is called, then validate should return null`() {
+        // Given
+        val givenEmail = "turkergoksu@gmail.com"
+        val emailValidator = EmailValidator()
+
+        // When
+        val actualResult = emailValidator.validate(givenEmail)
+
+        // Then
+        val expectedResult = null
+        Truth.assertThat(actualResult).isEqualTo(expectedResult)
+    }
+}
