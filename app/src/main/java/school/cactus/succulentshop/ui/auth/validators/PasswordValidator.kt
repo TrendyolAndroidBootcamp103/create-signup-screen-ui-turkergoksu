07dd6aa71d40c966d8dbf2afa4cdf14632412b87
turkergoksu <turkergoksu@gmail.com>
2021-05-02 18:45:32 +0300

    Fix password special character bug referred in feedback.

--- app/src/main/java/school/cactus/succulentshop/ui/auth/validators/PasswordValidator.kt
+++ app/src/main/java/school/cactus/succulentshop/ui/auth/validators/PasswordValidator.kt
@@ -4,16 +4,17 @@ import school.cactus.succulentshop.R
 
 class PasswordValidator : Validator {
 
-    // Regex from https://stackoverflow.com/a/21456918/6771753
-    private val passwordRule: Regex by lazy {
-        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$".toRegex()
-    }
-
     override fun validate(field: String) = when {
         field.isEmpty() -> R.string.err_password_field_is_required
-        field.length <= 7 -> R.string.err_password_is_too_short
+        field.length < 8 -> R.string.err_password_is_too_short
         field.length > 40 -> R.string.err_password_is_too_long
-        field.matches(passwordRule).not() -> R.string.err_password_must_provide_conditions
+        field.isContainsRequiredCharacters().not() -> R.string.err_password_must_provide_conditions
         else -> null
     }
+
+    private fun String.isContainsRequiredCharacters() =
+                any { it.isDigit() } &&
+                any { it.isLowerCase() } &&
+                any { it.isUpperCase() } &&
+                any { (it.isDigit().not() && it.isLetter().not()) }
 }
